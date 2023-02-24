f34e8a8604a251de975036ee6f05367a605d08eb
João Ramos <joaopsramos231@gmail.com>
2023-02-24 00:21:58 -0300

    refactor part 1

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,99 @@
+mod pr;
+
 use colored::Colorize;
-use octocrab::{
-    models::{pulls::PullRequest, User},
-    Error::GitHub,
-    OctocrabBuilder, Page,
-};
-use regex::Regex;
+use octocrab::{models::User, OctocrabBuilder, Page};
 use std::{
     env,
     fmt::Display,
     io::{self, Write},
-    process::Command,
 };
 
 const GITHUB_TOKEN_VAR: &str = "GITHUB_TOKEN";
 const GITHUB_USER_VAR: &str = "GITHUB_USER";
-const YT_ISSUE_REGEX: &str = r"^\w+/(\w+-\d+)";
-const BASE_REGEX: &str = r":([\w-]+)/";
-const REPO_REGEX: &str = r"/([\w-]+).git";
 
 #[tokio::main]
 async fn main() {
-    let user = env::var(GITHUB_USER_VAR).unwrap_or_else(|_err| {
+    let user = get_user();
+    let token = get_token();
+
+    let mut pr = pr::PR::build();
+
+    println!("\n{}", "** Review PR **".blue());
+    println!("{pr}");
+
+    proceed_question();
+
+    let octocrab = OctocrabBuilder::new()
+        .personal_token(token)
+        .build()
+        .unwrap();
+
+    println!("\nCreating PR...");
+
+    if pr.create(&octocrab).await.is_err() {
+        exit_with_code(1)
+    }
+
+    println!("\nAssigning to you...");
+
+    pr.assign_self(&octocrab, &user).await;
+
+    let collaborators_resp = octocrab
+        .orgs(&pr.base)
+        .list_members()
+        .per_page(100)
+        .send()
+        .await;
+
+    match collaborators_resp {
+        Ok(collaborators) => {
+            let reviewers = get_selected_reviewers(collaborators);
+            let usernames: Vec<String> = reviewers.iter().map(|r| r.username.clone()).collect();
+
+            if reviewers.is_empty() {
+                println!("\nNo reviewers to request");
+            } else {
+                let assigness_resp = octocrab
+                    .pulls(&pr.base, &pr.repo)
+                    .request_reviews(pr.number.unwrap(), usernames, [])
+                    .await;
+
+                match assigness_resp {
+                    Ok(_) => println!("\n{}", "Reviewers requested successfully".green()),
+                    Err(_) => println!("{}", "Failed to request reviewers".red()),
+                }
+            }
+        }
+        Err(_) => {
+            println!("\n{}", "Error fetching collaborators, ignoring...".red());
+        }
+    }
+
+    println!("\nPR: {}", pr.link.unwrap())
+}
+
+fn get_user() -> String {
+    env::var(GITHUB_USER_VAR).unwrap_or_else(|_err| {
         println!(
             "{}",
             format!("Couldn't get {} environment variable", GITHUB_USER_VAR).red()
         );
         exit_with_code(1);
-    });
-    let token = match env::var(GITHUB_TOKEN_VAR) {
-        Ok(token) => token,
-        Err(_) => {
-            println!(
-                "{}",
-                format!("Couldn't get {} environment variable", GITHUB_TOKEN_VAR).red()
-            );
-            println!("Please ensure the variable is available and it is a valid token");
-            exit_with_code(1);
-        }
-    };
+    })
+}
 
-    let remote_url = get_remote_url();
-    let base = get_base(&remote_url);
-    let repo = get_repo(&remote_url);
+fn get_token() -> String {
+    env::var(GITHUB_TOKEN_VAR).unwrap_or_else(|_err| {
+        println!(
+            "{}",
+            format!("Couldn't get {} environment variable", GITHUB_TOKEN_VAR).red()
+        );
+        println!("Please ensure the variable is available and it is a valid token");
+        exit_with_code(1);
+    })
+}
 
-    print!("\n");
-
-    let current_branch = get_current_branch();
-    let pr_title = get_pr_title();
-    let yt_issue = match get_yt_issue_from_branch_name(&current_branch) {
-        Some(yt_issue) => yt_issue,
-        None => {
-            println!(
-                "\n{}",
-                "Couldn't get Youtrack issue from branch name. Please provide one or leave it empty".red()
-            );
-            request_yt_issue()
-        }
-    };
-    let pr_body = get_pr_body();
-    let full_pr_body = build_full_pr_body(&pr_body, &yt_issue);
-
-    println!("\n{}", "** Review PR **".blue());
-    println!("Title: {}", pr_title.cyan());
-    println!("Body: {}", pr_body.cyan());
-    println!("Youtrack issue: {}", yt_issue.cyan());
-    println!("Remote branch: {}", current_branch.cyan());
-    println!("Remote: {}", format!("{}/{}", base, repo).cyan());
-
+fn proceed_question() {
     print!("\n{}", "Proceed? (y/n): ".yellow());
     flush_line();
 
@@ -93,222 +120,6 @@ async fn main() {
             }
         }
     }
-
-    println!("\nCreating PR...");
-
-    let octocrab = OctocrabBuilder::new()
-        .personal_token(token)
-        .build()
-        .unwrap();
-
-    let pr_resp = octocrab
-        .pulls(&base, &repo)
-        .create(pr_title, &current_branch, "next")
-        .body(full_pr_body)
-        .send()
-        .await;
-
-    let pr = match pr_resp {
-        Ok(pr) => {
-            let pr_link = get_pr_link(&pr);
-
-            print!("\n{}", "PR created successfully: ".green());
-            println!("{}", pr_link);
-
-            pr
-        }
-        Err(GitHub { source, .. }) => {
-            println!("\n{}", "Something went wrong, error message: ".red());
-            println!("{source}");
-            exit_with_code(1);
-        }
-        err => panic!("{:?}", err),
-    };
-
-    println!("\nAssigning to you...");
-
-    let assign_resp = octocrab
-        .issues(&base, &repo)
-        .add_assignees(pr.number, &[&user])
-        .await;
-
-    match assign_resp {
-        Ok(_) => println!("\n{}", "Assigned successfully".green()),
-        Err(_) => println!("\n{}", "Error when assigning".red()),
-    }
-
-    let collaborators_resp = octocrab
-        .orgs(&base)
-        .list_members()
-        .per_page(100)
-        .send()
-        .await;
-
-    match collaborators_resp {
-        Ok(collaborators) => {
-            let reviewers = get_selected_reviewers(collaborators);
-            let usernames: Vec<String> = reviewers.iter().map(|r| r.username.clone()).collect();
-
-            if reviewers.is_empty() {
-                println!("\nNo reviewers to request");
-            } else {
-                let assigness_resp = octocrab
-                    .pulls(&base, &repo)
-                    .request_reviews(pr.number, usernames, [])
-                    .await;
-
-                match assigness_resp {
-                    Ok(_) => println!("\n{}", "Reviewers requested successfully".green()),
-                    Err(_) => println!("{}", "Failed to request reviewers".red()),
-                }
-            }
-        }
-        Err(_) => {
-            println!("\n{}", "Error fetching collaborators, ignoring...".red());
-        }
-    }
-
-    println!("\nPR: {}", get_pr_link(&pr))
-}
-
-fn get_current_branch() -> String {
-    let stdout = Command::new("git")
-        .arg("branch")
-        .arg("--show-current")
-        .output()
-        .expect("failed to run `git branch --show-current`")
-        .stdout;
-
-    let current_branch = String::from_utf8(stdout).unwrap();
-
-    current_branch.trim().to_owned()
-}
-
-fn get_yt_issue_from_branch_name(branch: &str) -> Option<String> {
-    let re = Regex::new(YT_ISSUE_REGEX).unwrap();
-    let issue = re.captures(branch)?.get(1)?.as_str();
-
-    Some(issue.to_owned())
-}
-
-fn request_yt_issue() -> String {
-    let mut issue = String::new();
-
-    print!("Youtrack issue: ");
-    flush_line();
-
-    io::stdin().read_line(&mut issue).unwrap();
-
-    issue.trim().to_owned()
-}
-
-fn get_pr_title() -> String {
-    let last_commit = get_last_commit();
-    let mut pr_title = String::new();
-
-    println!("PR title: {}", last_commit.purple());
-    print!("Leave it blank to use the title above or digit a new one: ");
-    flush_line();
-
-    io::stdin().read_line(&mut pr_title).unwrap();
-
-    if pr_title.trim().is_empty() {
-        last_commit.to_string()
-    } else {
-        pr_title.trim().to_owned()
-    }
-}
-
-fn get_last_commit() -> String {
-    let stdout = Command::new("git")
-        .arg("log")
-        .arg("-1")
-        .arg("--pretty=format:%s")
-        .output()
-        .expect("failed to run `git log -1 --pretty=%s`")
-        .stdout;
-
-    let current_branch = String::from_utf8(stdout).unwrap();
-
-    current_branch.trim().to_owned()
-}
-
-fn get_pr_body() -> String {
-    let default_body = "Title".to_owned();
-    let mut pr_body = String::new();
-
-    println!("\nPR body: {}", default_body.purple());
-    print!("Leave it blank to use the body above or digit a new one: ");
-    flush_line();
-
-    io::stdin().read_line(&mut pr_body).unwrap();
-
-    if pr_body.trim().is_empty() {
-        default_body
-    } else {
-        pr_body.trim().to_owned()
-    }
-}
-fn build_full_pr_body(title: &str, yt_issue: &str) -> String {
-    format!("\
-### What does this PR do?
-
-{}
-
-<!--
-Please include a summary of the change and/or which issue is fixed. Please also include relevant motivation and context. List any dependencies that are required for this change, also provide (if appropriate) any evidence - screenshots, gifs, logs, etc.
-
-Oh, remember to follow conventional commits (https://conventionalcommits.org) on pull request title ;)
--->
-
----
-
-**Related issue:** {}
-", title, yt_issue)
-}
-
-fn get_remote_url() -> String {
-    let stdout = Command::new("git")
-        .arg("config")
-        .arg("--get")
-        .arg("remote.origin.url")
-        .output()
-        .expect("failed to run `git config --get remote.origin.url`")
-        .stdout;
-
-    let remote_url = String::from_utf8(stdout).unwrap();
-
-    remote_url.trim().to_owned()
-}
-
-fn get_base(remote_url: &str) -> String {
-    let err = format!(
-        "Failed to get the user/org name from remote url: {}",
-        remote_url
-    );
-    let re = Regex::new(BASE_REGEX).unwrap();
-
-    re.captures(remote_url)
-        .expect(&err)
-        .get(1)
-        .expect(&err)
-        .as_str()
-        .to_owned()
-}
-
-fn get_repo(remote_url: &str) -> String {
-    let err = format!(
-        "Failed to get the repo name from remote url: {}",
-        remote_url
-    );
-    let re = Regex::new(REPO_REGEX).unwrap();
-
-    re.captures(remote_url)
-        .expect(&err)
-        .get(1)
-        .expect(&err)
-        .as_str()
-        .to_owned()
 }
 
 #[derive(Debug, Clone)]
@@ -380,17 +191,6 @@ fn get_selected_reviewers(collaborators: Page<User>) -> Vec<Reviewer> {
         .into_iter()
         .filter(|r| r.selected)
         .collect()
-}
-
-fn get_pr_link(pr: &PullRequest) -> String {
-    let html_url = pr.html_url.as_ref().unwrap();
-
-    format!(
-        "{}://{}{}",
-        html_url.scheme(),
-        html_url.host().unwrap(),
-        html_url.path()
-    )
 }
 
 fn exit_with_code(code: i32) -> ! {
---
+++ src/pr.rs
@@ -0,0 +1,275 @@
+use colored::Colorize;
+use octocrab::{models::pulls::PullRequest, Error::GitHub, Octocrab};
+use regex::Regex;
+use std::{fmt::Display, io, process::Command};
+
+use crate::flush_line;
+
+const YT_ISSUE_REGEX: &str = r"^\w+/(\w+-\d+)";
+const BASE_REGEX: &str = r":([\w-]+)/";
+const REPO_REGEX: &str = r"/([\w-]+).git";
+
+pub struct PR {
+    pub branch: String,
+    pub title: String,
+    pub yt_issue: String,
+    pub body: String,
+    pub full_body: String,
+    pub link: Option<String>,
+    pub number: Option<u64>,
+    pub base: String,
+    pub repo: String,
+}
+
+impl PR {
+    pub fn build() -> Self {
+        let remote_url = get_remote_url();
+        let base = get_base(&remote_url);
+        let repo = get_repo(&remote_url);
+
+        print!("\n");
+
+        let current_branch = get_current_branch();
+        let title = get_pr_title();
+        let yt_issue = get_yt_issue(&current_branch);
+        let body = get_pr_body();
+        let full_body = build_full_pr_body(&body, &yt_issue);
+
+        PR {
+            branch: current_branch,
+            title,
+            yt_issue,
+            body,
+            full_body,
+            base,
+            repo,
+            link: None,
+            number: None,
+        }
+    }
+
+    pub async fn create(&mut self, octocrab: &Octocrab) -> Result<(), ()> {
+        let pr_resp = octocrab
+            .pulls(&self.base, &self.repo)
+            .create(&self.title, &self.branch, "next")
+            .body(&self.full_body)
+            .send()
+            .await;
+
+        match pr_resp {
+            Ok(github_pr) => {
+                self.number = Some(github_pr.number);
+                self.link = Some(get_pr_link(&github_pr));
+
+                print!("\n{}", "PR created successfully: ".green());
+                println!("{}", self.link.as_ref().unwrap());
+
+                Ok(())
+            }
+            Err(GitHub { source, .. }) => {
+                println!("\n{}", "Something went wrong, error message: ".red());
+                println!("{source}");
+
+                Err(())
+            }
+            err => panic!("{:?}", err),
+        }
+    }
+
+    pub async fn assign_self(&self, octocrab: &Octocrab, user: &str) {
+        let assign_resp = octocrab
+            .issues(&self.base, &self.repo)
+            .add_assignees(self.number.unwrap(), &[&user])
+            .await;
+
+        match assign_resp {
+            Ok(_) => println!("\n{}", "Assigned successfully".green()),
+            Err(_) => println!("\n{}", "Error when assigning".red()),
+        }
+    }
+}
+
+impl Display for PR {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let result = format!(
+            "\
+Title: {}
+Body: {}
+Youtrack issue: {}
+Remote branch: {}
+Remote: {}",
+            self.title.cyan(),
+            self.body.cyan(),
+            self.yt_issue.cyan(),
+            self.branch.cyan(),
+            format!("{}/{}", self.base, self.repo).cyan()
+        );
+
+        write!(f, "{}", result)
+    }
+}
+
+fn get_remote_url() -> String {
+    let stdout = Command::new("git")
+        .arg("config")
+        .arg("--get")
+        .arg("remote.origin.url")
+        .output()
+        .expect("failed to run `git config --get remote.origin.url`")
+        .stdout;
+
+    let remote_url = String::from_utf8(stdout).unwrap();
+
+    remote_url.trim().to_owned()
+}
+
+fn get_base(remote_url: &str) -> String {
+    let err = format!(
+        "Failed to get the user/org name from remote url: {}",
+        remote_url
+    );
+    let re = Regex::new(BASE_REGEX).unwrap();
+
+    re.captures(remote_url)
+        .expect(&err)
+        .get(1)
+        .expect(&err)
+        .as_str()
+        .to_owned()
+}
+
+fn get_repo(remote_url: &str) -> String {
+    let err = format!(
+        "Failed to get the repo name from remote url: {}",
+        remote_url
+    );
+    let re = Regex::new(REPO_REGEX).unwrap();
+
+    re.captures(remote_url)
+        .expect(&err)
+        .get(1)
+        .expect(&err)
+        .as_str()
+        .to_owned()
+}
+
+fn get_current_branch() -> String {
+    let stdout = Command::new("git")
+        .arg("branch")
+        .arg("--show-current")
+        .output()
+        .expect("failed to run `git branch --show-current`")
+        .stdout;
+
+    let current_branch = String::from_utf8(stdout).unwrap();
+
+    current_branch.trim().to_owned()
+}
+
+fn get_yt_issue(branch: &str) -> String {
+    match get_yt_issue_from_branch_name(&branch) {
+        Some(yt_issue) => yt_issue,
+        None => {
+            println!(
+                "\n{}",
+                "Couldn't get Youtrack issue from branch name. Please provide one or leave it empty".red()
+            );
+            request_yt_issue()
+        }
+    }
+}
+
+fn get_yt_issue_from_branch_name(branch: &str) -> Option<String> {
+    let re = Regex::new(YT_ISSUE_REGEX).unwrap();
+    let issue = re.captures(branch)?.get(1)?.as_str();
+
+    Some(issue.to_owned())
+}
+
+fn request_yt_issue() -> String {
+    let mut issue = String::new();
+
+    print!("Youtrack issue: ");
+    flush_line();
+
+    io::stdin().read_line(&mut issue).unwrap();
+
+    issue.trim().to_owned()
+}
+
+fn get_pr_title() -> String {
+    let last_commit = get_last_commit();
+    let mut pr_title = String::new();
+
+    println!("PR title: {}", last_commit.purple());
+    print!("Leave it blank to use the title above or digit a new one: ");
+    flush_line();
+
+    io::stdin().read_line(&mut pr_title).unwrap();
+
+    if pr_title.trim().is_empty() {
+        last_commit.to_string()
+    } else {
+        pr_title.trim().to_owned()
+    }
+}
+
+fn get_last_commit() -> String {
+    let stdout = Command::new("git")
+        .arg("log")
+        .arg("-1")
+        .arg("--pretty=format:%s")
+        .output()
+        .expect("failed to run `git log -1 --pretty=%s`")
+        .stdout;
+
+    let current_branch = String::from_utf8(stdout).unwrap();
+
+    current_branch.trim().to_owned()
+}
+
+fn get_pr_body() -> String {
+    let default_body = "Title".to_owned();
+    let mut pr_body = String::new();
+
+    println!("\nPR body: {}", default_body.purple());
+    print!("Leave it blank to use the body above or digit a new one: ");
+    flush_line();
+
+    io::stdin().read_line(&mut pr_body).unwrap();
+
+    if pr_body.trim().is_empty() {
+        default_body
+    } else {
+        pr_body.trim().to_owned()
+    }
+}
+
+fn build_full_pr_body(title: &str, yt_issue: &str) -> String {
+    format!("\
+### What does this PR do?
+
+{}
+
+<!--
+Please include a summary of the change and/or which issue is fixed. Please also include relevant motivation and context. List any dependencies that are required for this change, also provide (if appropriate) any evidence - screenshots, gifs, logs, etc.
+
+Oh, remember to follow conventional commits (https://conventionalcommits.org) on pull request title ;)
+-->
+
+---
+
+**Related issue:** {}
+", title, yt_issue)
+}
+
+fn get_pr_link(pr: &PullRequest) -> String {
+    let html_url = pr.html_url.as_ref().unwrap();
+
+    format!(
+        "{}://{}{}",
+        html_url.scheme(),
+        html_url.host().unwrap(),
+        html_url.path()
+    )
+}
